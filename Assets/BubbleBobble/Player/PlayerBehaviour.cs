12536ef2d3587a2ff1940ad646aa03ed59ac7589
Toarulle <rallen@live.se>
2023-10-11 16:46:05 +0200

    Tests, new platforms, animations, shoot,
    - Movement tests
      - Move Left
      - Move Right
      - Jump
      - Fall
    - Shoot test
      - Made through TDD
    - Collisions were bad on Tilemap collider, added separate gameobjects with colliders for the platforms
    - Added animations for moving, jumping, falling, shooting

--- Assets/BubbleBobble/Player/PlayerBehaviour.cs
+++ Assets/BubbleBobble/Player/PlayerBehaviour.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private InputActionReference movement, jump, shoot;
     [SerializeField] private Rigidbody2D rb = null;
+    [SerializeField] private Animator anim = null;
+    [SerializeField] private GameObject bubblePrefab = null;
     
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForce;
@@ -16,15 +19,42 @@ public class Player : MonoBehaviour
     
 
     private float movementInput;
-    public Vector2 currentVelocity;
 
     private bool isFacingRight = true;
+    private bool isJumping = false;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int IsJumping = Animator.StringToHash("IsJumping");
+    private static readonly int IsFalling = Animator.StringToHash("IsFalling");
+    private static readonly int ShootTrigger = Animator.StringToHash("Shoot");
 
     void Update()
     {
         rb.velocity = new Vector2(movementInput * movementSpeed, rb.velocity.y);
 
-        if (rb.velocity.y < 0f) rb.velocity = new Vector2 (rb.velocity.x, maxFallSpeed);
+        if (rb.velocity.y < 0f)
+        {
+            rb.velocity = new Vector2 (rb.velocity.x, maxFallSpeed);
+            anim.SetBool(IsFalling, true);
+            anim.SetBool(IsJumping, false);
+            isJumping = false;
+        }
+        else
+        {
+            anim.SetBool(IsFalling, false);
+        }
+
+        if (IsOnGround() && !isJumping)
+        {
+            anim.SetBool(IsFalling, false);
+            if (Math.Abs(rb.velocity.x) > 0f)
+            {
+                anim.SetBool(IsWalking, true);
+            }
+            else
+            {
+                anim.SetBool(IsWalking, false);
+            }
+        }
         if (!isFacingRight && movementInput > 0f)
         {
             Flip();
@@ -40,6 +70,8 @@ public class Player : MonoBehaviour
         if (context.performed && IsOnGround())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            anim.SetBool(IsJumping, true);
+            isJumping = true;
         }
 
         if (context.canceled && rb.velocity.y > 0f)
@@ -52,7 +84,16 @@ public class Player : MonoBehaviour
         movementInput = context.ReadValue<Vector2>().x;
     }
 
-    private bool IsOnGround()
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            anim.SetTrigger(ShootTrigger);
+            Instantiate(bubblePrefab, transform.position, Quaternion.identity);
+        }
+    }
+    
+    public bool IsOnGround()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
---
+++ Assets/IntegrationTests/PlayerTests.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.InputSystem;
+
+[TestFixture]
+public class PlayerTests
+{
+    private string sceneName = "PlayerScene";
+    private Scene scene = new Scene();
+    private PlayerBehaviour player = null;
+    
+    private InputTestFixture input = null;
+    private Keyboard keyboard = null;
+
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        input = new InputTestFixture();
+        input.Setup();
+        keyboard = InputSystem.AddDevice<Keyboard>();
+        yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        scene = SceneManager.GetSceneByName(sceneName);
+        SceneManager.SetActiveScene(scene);
+        player = Object.FindObjectOfType<PlayerBehaviour>();
+        yield return null;
+    }
+
+    [UnityTearDown]
+    public IEnumerator TearDown()
+    {
+        yield return SceneManager.UnloadSceneAsync(scene);
+        input.TearDown();
+        input = null;
+        keyboard = null;
+        player = null;
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator PlayerFalls()
+    {
+        float y0 = player.transform.position.y;
+        yield return new WaitForFixedUpdate();
+        float y1 = player.transform.position.y;
+        Assert.Less(y1,y0); //Test Player second Y position is less than first (falling)
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerInput_UpKeyPressed_PlayerJumps()
+    {
+        while (!player.IsOnGround())
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        float y0 = player.transform.position.y;
+        input.Press(keyboard.wKey);
+        yield return new WaitForSeconds(0.1f);
+        float y1 = player.transform.position.y;
+        Assert.Less(y0,y1); //Test Player first y position is less than second (has jumped)
+        yield return null;    
+    }
+    
+    [UnityTest]
+    public IEnumerator PlayerInput_SpaceKeyPressed_PlayerShoots()
+    {
+        var bubble = GameObject.FindAnyObjectByType<Bubble>();
+        Assert.Null(bubble);    //Test that no Bubble exists (before space has been pressed)
+        input.Press(keyboard.spaceKey);
+        yield return new WaitForSeconds(0.1f);
+        bubble = GameObject.FindAnyObjectByType<Bubble>();
+        var bubbles = GameObject.FindObjectsOfType<Bubble>();
+        Assert.NotNull(bubble); //Test that Bubble exists (after space has been pressed)
+        Assert.AreEqual(1,bubbles.Length);
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator PlayerInput_RightKeyPressed_PlayerMovesRight()
+    {
+        float x0 = player.transform.position.x;
+        input.Press(keyboard.dKey);
+        yield return new WaitForSeconds(0.1f);
+        float x1 = player.transform.position.x;
+        Assert.Less(x0,x1); //Test Player first X position is less than second (moved right)
+        yield return null;
+    }
+    
+    [UnityTest]
+    public IEnumerator PlayerInput_LeftKeyPressed_PlayerMovesLeft()
+    {
+        float x0 = player.transform.position.x;
+        input.Press(keyboard.aKey);
+        yield return new WaitForSeconds(0.1f);
+        float x1 = player.transform.position.x;
+        Assert.Less(x1,x0); //Test Player second X position is less than first (moved left)
+        yield return null;
+    }
+    
+    
+}
